850335937ba087d573e59f65152ccda9911a3559
cahyo arissabarno <cahyoarissabarno@gmail.com>
2021-12-27 17:54:19 +0700

    fix oauth2

--- routes/user.js
+++ routes/user.js
@@ -137,43 +137,41 @@ router.put('/reset-password', async(req,res)=>{
     //       pass: process.env.MAIL_KEY // generated ethereal password
     //     }
     // });
-
-    const createTransporter = async () => {
-        const oauth2Client = new OAuth2(
-          process.env.CLIENT_ID,
-          process.env.CLIENT_SECRET,
-          "https://developers.google.com/oauthplayground"
-        );
-      
-        oauth2Client.setCredentials({
-          refresh_token: process.env.REFRESH_TOKEN
-        });
-      
-        const accessToken = await new Promise((resolve, reject) => {
-          oauth2Client.getAccessToken((err, token) => {
-            if (err) {
-              reject();
-            }
-            resolve(token);
-          });
-        });
-      
-        const transporter = nodemailer.createTransport({
-          service: "gmail",
-          auth: {
-            type: "OAuth2",
-            user: process.env.EMAIL,
-            accessToken,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REFRESH_TOKEN
-          }
-        });
-      
-        return transporter;
-    }
-
     try {
+        const createTransporter = async () => {
+            const oauth2Client = new OAuth2(
+            process.env.CLIENT_ID,
+            process.env.CLIENT_SECRET,
+            "https://developers.google.com/oauthplayground"
+            );
+        
+            oauth2Client.setCredentials({
+            refresh_token: process.env.REFRESH_TOKEN
+            });
+        
+            const accessToken = await new Promise((resolve, reject) => {
+            oauth2Client.getAccessToken((err, token) => {
+                if (err) {
+                reject();
+                }
+                resolve(token);
+            });
+            });
+        
+            const transporter = nodemailer.createTransport({
+            service: "gmail",
+            auth: {
+                type: "OAuth2",
+                user: process.env.EMAIL,
+                accessToken,
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REFRESH_TOKEN
+            }
+            });
+        
+            return transporter;
+        }
         // const sendEmail = await transporter.sendMail(templateEmail)
         //emailOptions - who sends what to whom
         const sendEmail = async (emailOptions) => {
